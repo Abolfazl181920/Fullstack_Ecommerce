44040a49208a9012275dfaa3ad8d42f0dd382af1
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-03-31 15:16:23 +0300

    [ pages ]: created pages and import all in single constant!

---
+++ client/src/constants/pages.jsx
@@ -0,0 +1,17 @@
+import Home from '../pages/Home'
+import Address from '../pages/Address'
+import Bag from '../pages/Bag'
+import Checkout from '../pages/Checkout'
+import Dashboard from '../pages/Dashboard'
+import Payment from '../pages/Payment'
+import Signup from '../pages/Signup'
+
+export default {
+    Home,
+    Address,
+    Bag,
+    Checkout,
+    Dashboard,
+    Payment,
+    Signup
+}
