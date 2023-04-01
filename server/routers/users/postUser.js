2866bfd4ce3d33b64abc4d212144b15a26e34f05
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-04-01 20:28:10 +0300

    [ router ]: recreated user router (had bug)!

--- server/routers/users/postUser.js
+++ server/routers/users/postUser.js
@@ -1,58 +1,24 @@
 const { Router } = require('express')
 const { pool } = require("../../config/db.config")
 const router = Router()
-const bcrypt = require("bcrypt")
 
 router.post("/signup", (req, res) => {
-    const username = req.body.username
-    const email = req.body.email
-    const password = req.body.password
+    const q = 'INSERT INTO ecommerce.users (`userName`, `userPassword`, `userEmail`) VALUES (?)'
+    const user = [
+        req.body.userName,
+        req.body.userPassword,
+        req.body.userEmail,
+    ]
 
-    if (!username || !email || !password) {
-        return res.status(400).json({ message: 'Please fill in all fields' });
-    }
-
-    function hashPassword(password, callback) {
-        // Generate a salt
-        bcrypt.genSalt(10, (err, salt) => {
-          if (err) return callback(err)
-      
-          // Hash the password using the generated salt
-        bcrypt.hash(password, salt, (err, hash) => {
-            if (err) return callback(err)
-            callback(null, hash)
-            })
-        })
-    }      
-
-    hashPassword(password, (err, hashedPassword) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send('Internal Server Error')
-        }
-
-        const newUser = {
-            username,
-            email,
-            password: hashedPassword
-        }
-
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return res.status(500).json({ message: 'Internal server error' })
-            }
-
-            connection.query('INSERT INTO users SET ?', newUser, (error) => {
-                connection.release()
-
-                if (error) {
-                    return res.status(500).json({ message: 'Internal server error' })
-                }
-
-                res.status(201).json({ message: 'User created successfully' })
-            })
-        })
+    pool.query(q, [ user ], (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
     })
+
+    // if (!userName || !userEmail || !userPassword) {
+    //     return res.status(400).json({ message: 'Please fill in all fields' });
+    // }
 })
+
 
 module.exports = router
